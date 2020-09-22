a7aff52cea06afa9eb84bd7c527748f3edb6696b
jacksolaro <jacksolaro@gmail.com>
2020-09-21 18:19:48 -0700

    set up html layout using bootstrap

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
+// Variables
+var city = $("#search-city");
+var searchBtn = $("#search-button")
 
-
-
-// TODO
 
 // TODO: Connect to openweather API (AJAX request)
 
-var city = "Seattle";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=dde55275793968119ad3b1dd2529eda8";
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  // Create a new table row element
-  console.log(response);
-});
 
 
+searchBtn.on("click", function(e){
+    e.preventDefault();
+    
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city.val() + "&appid=dde55275793968119ad3b1dd2529eda8";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // Grab Latitude and Longitude from the city in the first API call
+        var inputLat = response.coord.lat;
+        var inputLon = response.coord.lon;
+        var tempF = (response.main.temp * 9/5) - 459.67
+        var humidity = response.main.humidity;
+        var windSpeed = response.wind.speed;
+        var uvIndex;
+        
+    
+        // Return the UV Index using the Latitude and Longitude from the first API call
+        $.ajax({ 
+            url: `https://api.openweathermap.org/data/2.5/uvi?appid=dde55275793968119ad3b1dd2529eda8&lat=${inputLat}&lon=${inputLon}`,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        })
+    
+    
+        //   TODO: Create Weather Card with response data
+        var weatherDiv = $("<div>");
+        
 
-// TODO: Pass user city input to openweather API
+
+        console.log(response);
+    });
+
+})
+
+
+
 
 // TODO: Retrieve from API: Current (Temperature, Humidity, wind speed, UV index) 
 
